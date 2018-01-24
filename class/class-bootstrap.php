acd61e4da522ae307883579b8fa1391b8067c947
Eric Fuller <info@glassmoonmedia.com>
2018-01-24 07:53:02 -0500

    Bind plugin details to the App.

--- class/class-bootstrap.php
+++ class/class-bootstrap.php
@@ -16,11 +16,30 @@ namespace WPSP;
 class Bootstrap {
 
 	/**
+	 * Current version.
+	 *
+	 * @var string
+	 * @since 1.0.0
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Main plugin file.
+	 *
+	 * @var string
+	 * @since 1.0.0
+	 */
+	protected $plugin_file = '';
+
+	/**
 	 * Constructor. Initialize the plugin.
 	 *
 	 * @since 1.0.0
 	 */
 	public function __construct() {
+
+		$this->plugin_file = dirname( __FILE__ );
+
 		add_action( 'init', array( $this, 'init' ) );
 	}
 
@@ -30,6 +49,10 @@ class Bootstrap {
 	 * @since 1.0.0
 	 */
 	public function init() {
+		App::bind( 'version', self::VERSION );
+		App::bind( 'plugin_directory_url', plugin_dir_url( $this->plugin_file ) );
+		App::bind( 'plugin_directory_path', plugin_dir_path( $this->plugin_file ) );
+		App::bind( 'basename', basename( dirname( $this->plugin_file ) ) );
 		App::bind( 'dependencies', new Dependencies() );
 	}
 }
